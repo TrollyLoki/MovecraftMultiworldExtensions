8f3e8d18ebf3b8373745573ea6d9dead91db1c92
TrollyLoki <trollyloki@gmail.com>
2025-03-04 15:44:56 -0500

    Fix crafts trying to switch worlds despite not actually passing through a switch height

--- src/main/java/net/trollyloki/movecraftmultiworldextensions/HeightSwitchingListener.java
+++ src/main/java/net/trollyloki/movecraftmultiworldextensions/HeightSwitchingListener.java
@@ -16,21 +16,23 @@ public class HeightSwitchingListener implements Listener {
 	@EventHandler
 	public void onPreTranslateEvent(CraftPreTranslateEvent event) {
 		if (MovecraftMultiworldExtensions.doHeightSwitching) {
-			
+
 			String craftWorld = event.getWorld().getName();
 			Map<String, Integer> switchHeights = heights.get(craftWorld);
-			int maxY = event.getCraft().getHitBox().getMaxY() + event.getDy();
-			int minY = event.getCraft().getHitBox().getMinY() + event.getDy();
-			
+			int maxY = event.getCraft().getHitBox().getMaxY();
+			int minY = event.getCraft().getHitBox().getMinY();
+
 			for (String world : switchHeights.keySet()) {
 				int switchHeight = switchHeights.get(world);
-				if (maxY >= switchHeight || minY <= switchHeights.get(world)) {
-					
+				if (maxY < switchHeight && maxY + event.getDy() >= switchHeight ||
+						minY > switchHeight && minY + event.getDy() <= switchHeight) {
+
 					event.setWorld(Bukkit.getWorld(world));
 					int destHeight = heights.get(world).get(craftWorld);
 					if (destHeight > switchHeight) event.setDy(destHeight - event.getCraft().getHitBox().getMaxY());
 					else event.setDy(destHeight - event.getCraft().getHitBox().getMinY());
-					
+
+					break;
 				}
 			}
 			

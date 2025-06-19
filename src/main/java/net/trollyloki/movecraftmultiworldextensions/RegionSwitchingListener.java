73ae56c640235b0b451b7cef20dd30c053f19395
TrollyLoki <trollyloki@gmail.com>
2025-06-19 12:41:23 -0400

    Fix crafts becoming stuck in region "portals" if the other side is blocked

--- src/main/java/net/trollyloki/movecraftmultiworldextensions/RegionSwitchingListener.java
+++ src/main/java/net/trollyloki/movecraftmultiworldextensions/RegionSwitchingListener.java
@@ -1,6 +1,7 @@
 package net.trollyloki.movecraftmultiworldextensions;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
@@ -26,9 +27,12 @@ public class RegionSwitchingListener implements Listener {
 		if (MovecraftMultiworldExtensions.doRegionSwitching) {
 			
 			RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
-			RegionManager rm = container.get(BukkitAdapter.adapt(event.getCraft().getWorld()));
+			RegionManager rm = container.get(BukkitAdapter.adapt(event.getWorld()));
 			
 			HitBox hitbox = event.getCraft().getHitBox();
+			List<MovecraftLocation> locations = hitbox.asSet().stream()
+					.map(location -> location.translate(event.getDx(), event.getDy(), event.getDz()))
+					.toList();
 			/*Vector pos1 = new com.sk89q.worldedit.Vector(hitbox.getMinX() + event.getDx(), hitbox.getMinY() + event.getDy(), hitbox.getMinZ() + event.getDz());
 			Vector pos2 = new com.sk89q.worldedit.Vector(hitbox.getMaxX() + event.getDx(), hitbox.getMaxY() + event.getDy(), hitbox.getMaxZ() + event.getDz());
 			CuboidRegion region = new com.sk89q.worldedit.regions.CuboidRegion(pos1, pos2);*/
@@ -36,7 +40,7 @@ public class RegionSwitchingListener implements Listener {
 			// Check each defined region to see if a block is in it - probably faster
 			for (String regionName : regions.keySet()) {
 				ProtectedRegion region = rm.getRegion(regionName);
-				if (region != null) for (MovecraftLocation location : hitbox) {
+				if (region != null) for (MovecraftLocation location : locations) {
 					if (region.contains(location.getX(), location.getY(), location.getZ())) {
 						event.setWorld(Bukkit.getWorld(regions.get(regionName)));
 						return;
